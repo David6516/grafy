ec3fe56a3ea9ce35f1d9afb933774a2c26a127e4
David Habryka <davehabryka@gmail.com>
2024-01-26 18:37:33 +0100

    Add implementation of searching for cycles in graph

--- graph.cpp
+++ graph.cpp
@@ -1,45 +1,79 @@
 #include "graph.hpp"
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-Graph::Graph(int maxNodeIncremented) /*: V(maxNodeIncremented), adj(maxNodeIncremented), hasCycle(false)*/ {
-    adj.resize(maxNodeIncremented);
+Graph::Graph(int maxNodeIncremented) : V(maxNodeIncremented), adj(maxNodeIncremented), hasCycle(false) {
+
+    // V = maxNodeIncremented;
+    // adj = vector<vector<int>>(maxNodeIncremented);
+    // hasCycle = false;
 }
 
-/**
- * @brief Adds an edge to the graph between two vertices.
- * 
- * @param u The source vertex.
- * @param v The destination vertex.
- */
-void Graph::addEdge(int u, int v) {
-    adj[u].push_back(v);
+void Graph::addEdge(int u, int v) 
+{
+    adj[u].insert(v);
 
-    // only for debugging, will be removed later
-    cout << "Added edge from " << u << " to " << v << endl;
-    cout << "Current adj list for " << u << ": ";
-
-    for (int i : adj[u]) {
-        cout << i << " ";
-    }
-    
-    // for (int j = 0; j < adj[u].size(); j++) {
-    //     cout << adj[u][j] << " ";
+    // only for data structure analysis, will be removed later
+    // cout << "Added edge from " << u << " to " << v << endl;
+    // cout << "Current adj list for " << u << ": ";
+    // for (int i : adj[u]) {
+    //     cout << i << " ";
     // }
- 
-    cout << endl;
+    // cout << endl;
 }
 
 
-
-/**
- * @brief Finds all cycles in the graph.
- * 
- * This method searches for all cycles in the graph using depth-first search algorithm.
- * (not implemented yet).
- * 
- */
 void Graph::findCycles() {
-    cout << "This will search for cycles, but is only stub for now..." << endl;
+    vector<bool> visited(V, false);
+    vector<int> path;
+
+    for (int i = 1; i <= V; i++) {
+        findCyclesUtil(i, visited, path);
+    }
+
+    if (!hasCycle) {
+        cout << "No cycles found.\n";
+    }
+}
+
+void Graph::findCyclesUtil(int node, vector<bool> &visited, vector<int> &path)
+{
+    visited[node] = true;
+    path.push_back(node);
+
+    for (int i : adj[node]) {
+        if (!visited[i]) {
+            findCyclesUtil(i, visited, path);
+        } else if (find(path.begin(), path.end(), i) != path.end()) {
+            printCycle(path, i);
+        }
+    }
+
+    path.pop_back();
+    visited[node] = false;
+}
+
+void Graph::printCycle(vector<int>& path, int v) {
+    auto it = find(path.begin(), path.end(), v);
+    vector<int> cycle(it, path.end());
+    
+    // this is wrong !!!
+    //sort(cycle.begin(), cycle.end());
+
+    // Rotate the cycle so that the smallest element is first
+    rotate(cycle.begin(), min_element(cycle.begin(), cycle.end()), cycle.end());
+
+    if (!cycles.count(cycle)) {
+        cycles.insert(cycle);
+        if (!hasCycle) {
+            cout << "Cycle(s) found:\n";
+            hasCycle = true;
+        }
+        for (int i : cycle) {
+            cout << i << ' ';
+        }
+        cout << '\n';
+    }
 }
--- graph.hpp
+++ graph.hpp
@@ -11,17 +11,21 @@ using namespace std;
  * @author David Habryka
  */
 class Graph {
+
     /**
-     * @brief adj is a vector of vectors to represent adjacency matrix.
+     * @brief adj is a vector of sets (to avoid repetitions) to represent adjacency matrix.
      */
-    vector<vector<int>> adj;
+
+    vector<set<int>> adj;
     /**
-     * @brief V is the number of vertices in the graph (+1 for convenience to avoid using 0 index).
+     * @brief V is the number of edges in the graph (+1 for convenience to avoid using 0 index).
      */
+
     int V;
     /**
      * @brief cycles is a set of vectors to store all cycles in the graph.
      */
+
     set<vector<int>> cycles;
     /**
      * @brief hasCycle is a boolean variable to check if the graph has any cycle or not.
@@ -30,26 +34,56 @@ class Graph {
 
 public:
     /**
-     * @brief Construct a new Graph object with given number of vertices.
+     * @brief Construct a new Graph object with given number of edges.
      * 
-     * @param maxNodeIncremented The number of vertices in the graph  (+1 for convenience to avoid using 0 index).
+     * @param maxNodeIncremented The number of edges in the graph  (+1 for convenience to avoid using 0 index).
      */
     Graph(int maxNodeIncremented);
 
     /**
-     * @brief Add an edge to the graph.
+     * @brief Adds an edge between two vertices in the graph.
      * 
-     * @param u The source vertex of the edge.
-     * @param v The destination vertex of the edge.
+     * @param u The source vertex.
+     * @param v The destination vertex.
      */
     void addEdge(int u, int v);
 
     /**
-     * @brief Find all cycles in the graph.
+     * @brief Finds cycles in the graph.
+     *
+     * This method performs a depth-first search to find cycles in the graph.
+     * It starts the search from each vertex in the graph and keeps track of visited vertices and the current path.
+     * If a cycle is found, it sets the `hasCycle` flag to true.
+     *
+     * @return void
      */
     void findCycles();
 
 private:
-    //TODO: some private helper methods may need to be added here
+    /**
+     * @brief Finds cycles in the graph starting from a given node.
+     * 
+     * This function performs a depth-first search to find cycles in the graph
+     * starting from the specified node. It uses a recursive approach to traverse
+     * the graph and keeps track of visited nodes and the current path.
+     * 
+     * @param node The starting node for the search.
+     * @param visited A vector indicating whether a node has been visited or not.
+     * @param path A vector representing the current path in the graph.
+     */
+    void findCyclesUtil(int v, vector<bool>& visited, vector<int>& path);
+
+    /**
+     * @brief Prints the cycle starting from the given node.
+     * 
+     * This function prints the cycle starting from the given node by finding the path in the vector and 
+     * extracting the cycle from it. The cycle is then sorted and printed to the console. If the cycle 
+     * is not already present in the set of cycles, it is added to the set and the hasCycle flag is set 
+     * to true.
+     * 
+     * @param path The vector containing the path.
+     * @param node The starting node of the cycle.
+     */
+    void printCycle(vector<int>& path, int v);
 
 };
